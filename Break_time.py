877b50464820c35855457d08d01dcd8787ef9d05
Patrick waweru <32388339+drunkingpato@users.noreply.github.com>
2018-02-27 23:13:34 +0300

    Update Break_time.py

--- Break_time.py
+++ Break_time.py
@@ -4,10 +4,10 @@ import webbrowser
 
 total_count=3
 count=0
-
+x=input("Enter the number of hours  to wait ")
 
 while(count<total_count):
 	print(" The program started at: "+time.ctime())
-	time.sleep(2*60*60)
+	time.sleep(x*60*60)
 	webbrowser.open("https://www.youtube.com/watch?v=DqmIT3dVQyY")
 	count=count+1
